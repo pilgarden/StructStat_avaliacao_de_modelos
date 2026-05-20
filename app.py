06780e482a01390f2d98aa46d89499fbaeb988bf
pilgarden <160924322+pilgarden@users.noreply.github.com>
2026-05-20 01:14:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,15 +101,15 @@ if df is not None:
         )
     elif not vars_ok:
         st.sidebar.caption("Selecione as variáveis acima para gerar o PDF.")
-    st.markdown(
-    """
-    <div style='text-align: center; color: gray; font-size: 0.85em;'>
-        <b>StructStat: módulo de avaliação de modelos</b><br>
-        Desenvolvido por: Pedro Jardim<br>
-        <i>v1.0 - [MÊS]/[ANO]</i>
-    </div>
-    """, 
-    unsafe_allow_html=True
+st.markdown(
+"""
+<div style='text-align: center; color: gray; font-size: 0.85em;'>
+    <b>StructStat: módulo de avaliação de modelos</b><br>
+    Desenvolvido por: Pedro Jardim<br>
+    <i>v1.0 - Maio/2026</i>
+</div>
+""", 
+unsafe_allow_html=True
 )
 # --- Área principal ---
 if df is not None:
